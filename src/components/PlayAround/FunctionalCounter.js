8ba58a014a793ddc5ceedd496b6d0996652000e5
Mohamed Malek <mohamed.khatwa.zaid@gmail.com>
2020-08-24 05:18:57 +0200

    Changed Formations

--- src/components/PlayAround/FunctionalCounter.js
+++ src/components/PlayAround/FunctionalCounter.js
@@ -1,28 +1,25 @@
 import React from 'react';
-import {View,Text, Button} from 'react-native';
+import { View, Text, Button } from 'react-native';
 
-export function Counter (props) {
+export function Counter(props) {
 
-    const [counter, setCounter] = React.useState (0);
+    const [counter, setCounter] = React.useState(0);
 
-
-     const incrementHandler = () =>{
-         setCounter(counter + 1);
-
+    const incrementHandler = () => {
+        setCounter(counter + 1);
     };
-    const decrementHandler = () =>{
+
+    const decrementHandler = () => {
         setCounter(counter - 1);
     };
 
+    return (
+        <View style={{ alignItems: 'center' }}>
+            <Button title='Increment' onPress={incrementHandler}></Button>
+            <Text>{counter}</Text>
+            <Button title='Decrement' onPress={decrementHandler}></Button>
+        </View>
+    );
 
-    
-        return(
-            <View style={{alignItems:'center'}}>
-                <Button title='Increment' onPress={incrementHandler}></Button>
-                <Text>{counter}</Text>
-                <Button title='Decrement' onPress={decrementHandler}></Button>
-            </View>
-        );
 
-    
 }

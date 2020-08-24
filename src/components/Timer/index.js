32e83eb6ee19509c925651e08c2d93fb47b937ac
Mohamed Malek <mohamed.khatwa.zaid@gmail.com>
2020-08-24 23:28:01 +0200

    Making Functionala Timer

--- src/components/Timer/index.js
+++ src/components/Timer/index.js
@@ -2,68 +2,33 @@ import React from 'react';
 import {Text} from 'react-native';
 import {formatTimer} from '../../utils/helperFunctions';
 
+export function Timer(props) {
+  const [seconds, setSeconds] = React.useState(
+    props.descending ? props.maxSeconds : 0,
+  );
 
+  let interval;
 
-export class Timer extends React.Component {
-    constructor(props) {
-        super(props);
+  React.useEffect(() => {
+    interval = setInterval(() => {
+      setSeconds(prevSeconds =>
+        props.descending ? prevSeconds - 1 : prevSeconds + 1,
+      );
+    }, 1000);
 
-        /* if Timer CountDown start From Max Seconds
-         if not start From 0 */
-        this.state = {
-            seconds: props.descending ? props.maxSeconds : 0,
-        };
+    return () => {
+      clearInterval(interval);
+    };
+  }, [seconds]);
+
+  React.useEffect(() => {
+    const deadlineVal = props.descending ? 0 : props.maxSeconds;
+
+    if (seconds === deadlineVal) {
+      clearInterval(interval);
+      props.deadlineFunction();
     }
+  }, [seconds]);
 
-    //make action when  the page load :
-
-    componentDidMount() {
-
-        //making Timer 
-
-        this.interval = setInterval(() =>{
-
-            
-
-            
-
-            //make Timer Count Seconds:
-
-            this.setState(pervstate => {
-                return {
-                    seconds: this.props.descending
-                    ? pervstate.seconds - 1
-                    : pervstate.seconds + 1,
-                };
-            })
-        },1000)
-    };
-
-
-    //make Timer stop After TimeOut:
-
-    componentDidUpdate(prevProps, prevState){
-
-        const DeadLine = this.props.descending ? 0
-        : this.props.maxSeconds
-
-        if (this.state.seconds === DeadLine){
-            clearInterval(this.interval);
-
-            // mace Action Whin Time out
-            this.props.deadLineFunction();
-            
-        };
-
-    };
-
-    // clear Timer (If the User colse the App Before Time Out)
-    componentWillUnmount(){
-        clearInterval(this.interval);
-    }
-
-    render() {
-        return <Text>{formatTimer(this.state.seconds)}</Text>
-    };
-    
-};
+  return <Text>{formatTimer(seconds)}</Text>;
+}

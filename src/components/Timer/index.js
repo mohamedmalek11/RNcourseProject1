84b9faba85fb5b201cc130a62b1da6dfafc49bdd
Mohamed Malek <mohamed.khatwa.zaid@gmail.com>
2020-08-24 23:40:41 +0200

    Added Timer Notes

--- src/components/Timer/index.js
+++ src/components/Timer/index.js
@@ -2,6 +2,9 @@ import React from 'react';
 import {Text} from 'react-native';
 import {formatTimer} from '../../utils/helperFunctions';
 
+
+/* the Timer Takes Three Props : maxSeconds , descending , deadlineFunction
+ Make sure you but them in app.js */
 export function Timer(props) {
   const [seconds, setSeconds] = React.useState(
     props.descending ? props.maxSeconds : 0,
@@ -9,6 +12,7 @@ export function Timer(props) {
 
   let interval;
 
+    // seconds COunter :
   React.useEffect(() => {
     interval = setInterval(() => {
       setSeconds(prevSeconds =>
@@ -16,16 +20,20 @@ export function Timer(props) {
       );
     }, 1000);
 
+    // Clear tThe interval If Client Close Before Time Out
     return () => {
       clearInterval(interval);
     };
   }, [seconds]);
 
+    // Stop Timer After Time Out
   React.useEffect(() => {
     const deadlineVal = props.descending ? 0 : props.maxSeconds;
 
     if (seconds === deadlineVal) {
       clearInterval(interval);
+
+      // Make Action After Time Out
       props.deadlineFunction();
     }
   }, [seconds]);

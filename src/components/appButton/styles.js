ab3c5fc7a19a33c6e6090787efc5a786fa622e0f
Mohamed Malek <mohamed.khatwa.zaid@gmail.com>
2020-08-25 18:45:08 +0200

    Changed Button Style

--- src/components/appButton/styles.js
+++ src/components/appButton/styles.js
@@ -9,7 +9,7 @@ const styles = StyleSheet.create({
     borderColor: buttonColor,
     borderRadius: 5,
     borderWidth: 1,
-    padding: 10,
+    padding: 15,
     justifyContent: 'center',
     alignItems: 'center',
   },

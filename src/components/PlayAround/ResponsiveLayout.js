b1e485568215eb6e5595292f9b6c594cf5e6aed8
Mohamed Malek <mohamed.khatwa.zaid@gmail.com>
2020-08-28 16:57:07 +0200

    Changed Font Size + Added Comments

--- src/components/PlayAround/ResponsiveLayout.js
+++ src/components/PlayAround/ResponsiveLayout.js
@@ -1,12 +1,13 @@
 import React, { useState, useEffect } from 'react';
 import { TextInput, View, Text, Dimensions } from 'react-native';
 
-const {width} = Dimensions.get('window')
+const { width } = Dimensions.get('window')
 
 export function Input(props) {
     return (
         <TextInput style={{
             height: 40,
+            fontSize: 18,
             borderColor: '#bbb',
             borderWidth: 1,
             borderRadius: 7,
@@ -17,44 +18,47 @@ export function Input(props) {
 
 };
 export function SignUpScreen(props) {
-    const [realWidth , SetWidth] = React.useState(width);
+    const [realWidth, SetWidth] = React.useState(width);
 
-        useEffect(() => {
-            Dimensions.addEventListener('change', ({window}) => {
-                SetWidth(window.width);
-            });
-            return() => {
-                Dimensions.removeEventListener('change');
-            };
+    //make View Change When Rotate:
+    useEffect(() => {
+        Dimensions.addEventListener('change', ({ window }) => {
+            SetWidth(window.width);
+        });
+        return () => {
+            Dimensions.removeEventListener('change');
+        };
 
-        }, [])
+    }, [])
+    //
+
 
     return (
         <View style={{
-            flex : 1,
+            flex: 1,
             justifyContent: 'center',
-            paddingHorizontal : 50,
-            
+            paddingHorizontal: 50,
+
         }}>
-        <View style = {realWidth > 500? {paddingHorizontal : 100,}: null}>
-            <Input placeholder={'Email'} />
+            <View style={realWidth > 500 ? { paddingHorizontal: 100, } : null}>
+                <Input placeholder={'Email'} />
 
-            <View style = {realWidth > 500 ?  {flexDirection : 'row'}: null}>
+                <View style={realWidth > 500 ? { flexDirection: 'row' } : null}>
 
-            <View style = {realWidth > 500? {flex: 1, marginEnd: 5, }: null}> 
-            <Input placeholder={'PassWord'} />
-            </View>
+                    <View style={realWidth > 500 ? { flex: 1, marginEnd: 5, } : null}>
+                        <Input placeholder={'Password'} />
+                    </View>
 
-            <View style = {realWidth > 500? {flex: 1, marginStart: 5,}: null}> 
-            <Input placeholder={'Confirm'} /> 
-            </View>
+                    <View style={realWidth > 500 ? { flex: 1, marginStart: 5, } : null}>
+                        <Input placeholder={'Confirm'} />
+                    </View>
 
-            </View>
+                </View>
 
-         <View style = {{alignItems: 'center'}}>
-          <Text style={{ alignContent: 'center' }}>Sign Up</Text>
-         </View>
-           
+                <View style={{ alignItems: 'center' }}>
+                    <Text style={{ alignContent: 'center', fontSize: 20, fontWeight: '400', }}>Sign Up</Text>
+                </View>
+
             </View>
         </View>
     );

fbbbf520b9a6b15b21c9e11ea11c1a00ab81b67d
Mohamed Malek <mohamed.khatwa.zaid@gmail.com>
2020-08-29 19:11:13 +0200

    Finished Input Componant

--- src/components/input/index.js
+++ src/components/input/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {TextInput, View} from 'react-native';
+import {TextInput, View, StyleSheet, Text} from 'react-native';
 
 export function Input(props) {
   const {
@@ -10,21 +10,52 @@ export function Input(props) {
     borderWidth,
     borderRadius,
     borderColor,
+    placeholderPosition,
+    renderIconLeft,
+    renderIconRight,
+    stacked,
+    placeholder,
+    placeholderColore,
+    style,
+    IconWrapperStyle,
+    WrapperStyle,
     ...rest
   } = props;
 
   return (
     <View
-      style={
+      style={[
         borderd && {
           borderWidth: borderWidth || 1,
           padding: 5,
           borderRadius: borderRadius || 5,
           borderColor: borderColor || '#bbb',
-        }
-      }>
-      <TextInput {...rest} style={{padding: 1}} />
-      {underlined && (
+        },
+        WrapperStyle,
+      ]}>
+      {stacked && (
+        <Text style={{color: placeholderColore || '#000'}}>{placeholder}</Text>
+      )}
+
+      <View
+        style={ {
+          flexDirection: 'row',
+          justifyContent: placeholderPosition || 'flex-start',
+        }}>
+        {renderIconLeft && (
+          <View style={[styles.IconWrapper,IconWrapperStyle]}>{renderIconLeft()}</View>
+        )}
+        <TextInput
+          placeholder={stacked ? '' : placeholder}
+          {...rest}
+          style={[{padding: 2}, style]}
+        />
+        {renderIconRight && (
+          <View style={styles.IconWrapper}>{renderIconRight()}</View>
+        )}
+      </View>
+
+      {(underlined || stacked) && (
         <View
           style={{
             height: underlinedHeight || 1,
@@ -36,3 +67,10 @@ export function Input(props) {
     </View>
   );
 }
+const styles = StyleSheet.create({
+  IconWrapper: {
+    fontSize: 50,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+});

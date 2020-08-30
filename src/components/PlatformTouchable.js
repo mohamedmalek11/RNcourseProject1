ae45046609be10432c01e46d877a5737cb5f435f
Mohamed Malek <mohamed.khatwa.zaid@gmail.com>
2020-08-30 05:25:15 +0200

    Changed Style

--- src/components/PlatformTouchable.js
+++ src/components/PlatformTouchable.js
@@ -1,12 +1,16 @@
 import React from 'react';
-import { TouchableOpacity, TouchableNativeFeedback, Platform } from 'react-native';
+import {
+  TouchableOpacity,
+  TouchableNativeFeedback,
+  Platform,
+} from 'react-native';
 
-
+//Chang Style For Every Platform (in App Button)
 export class PlatformTouchable extends React.Component {
-    render() {
-        const Touchable =
-            Platform.OS === 'android' ? TouchableNativeFeedback : TouchableOpacity;
+  render() {
+    const Touchable =
+      Platform.OS === 'android' ? TouchableNativeFeedback : TouchableOpacity;
 
-        return <Touchable {...this.props} />
-    }
+    return <Touchable {...this.props} />;
+  }
 }

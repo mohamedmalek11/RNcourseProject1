1dbb4060cc005f366479fb4d25dca5eb5e8418c1
Mohamed Malek <mohamed.khatwa.zaid@gmail.com>
2020-08-30 05:24:46 +0200

    Changed Style

--- src/components/appButton/index.js
+++ src/components/appButton/index.js
@@ -1,26 +1,23 @@
 import React from 'react';
-import { Text, ActivityIndicator } from 'react-native';
-import { PlatformTouchable } from '../PlatformTouchable';
+import {Text, ActivityIndicator} from 'react-native';
+import {PlatformTouchable} from '../PlatformTouchable';
 import styles from './styles';
 
 export class AppButton extends React.Component {
-    render() {
-        const { 
-            title,
-             WrapperStyle,
-             titleStyle,
-             isLoading, ...rest } = this.props;
+  render() {
+    const {title, WrapperStyle, titleStyle, isLoading, ...rest} = this.props;
 
-
-
-        return (
-            <PlatformTouchable {...rest} disabled={isLoading} style={[styles.wrapper, WrapperStyle]}>
-                {
-                    isLoading ? <ActivityIndicator /> : <Text style={[styles.title, titleStyle]}>{title}</Text>
-                }
-
-
-
-            </PlatformTouchable>)
-    }
+    return (
+      <PlatformTouchable
+        {...rest}
+        disabled={isLoading}
+        style={[styles.wrapper, WrapperStyle]}>
+        {isLoading ? (
+          <ActivityIndicator />
+        ) : (
+          <Text style={[styles.title, titleStyle]}>{title}</Text>
+        )}
+      </PlatformTouchable>
+    );
+  }
 }

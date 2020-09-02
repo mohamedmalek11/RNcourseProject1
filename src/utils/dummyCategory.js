17ea59f22b32f91ec8bd703f6b352fe3b751c912
Mohamed Malek <mohamed.khatwa.zaid@gmail.com>
2020-09-02 07:50:56 +0200

    Changed Name + Added Dummy Data

--- src/utils/dummyCategory.js
+++
@@ -1,4 +0,0 @@
-export const dummyCategory = {
-    title : "category",
-    imageUrl: 'https://reactjs.org/logo-og.png',
-}
---
+++ src/utils/dummyData.js
@@ -0,0 +1,18 @@
+export const dummyCategory = {
+    title : "category",
+    imageUrl: 'https://reactjs.org/logo-og.png',
+};
+
+export const dummyProductWithDiscount = {
+    title : "Product Product ProductProduct",
+    imageUrl: 'https://reactjs.org/logo-og.png',
+    price : 100,
+    discount: 0.2,
+};
+
+export const dummyProduct = {
+    title : "Product",
+    imageUrl: 'https://reactjs.org/logo-og.png',
+    price : 100,
+};
+

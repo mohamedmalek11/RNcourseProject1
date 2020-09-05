b43f95e44d5e72891188c203b962f615fd5e8b98
Mohamed Malek <mohamed.khatwa.zaid@gmail.com>
2020-09-05 20:17:35 +0200

    Added Dummy Products Data

--- src/utils/dummyData.js
+++ src/utils/dummyData.js
@@ -1,26 +1,109 @@
-export const dummyCategory = {
-    title : "category",
-    imageUrl: 'https://reactjs.org/logo-og.png',
+export const dummyCategory1 = {
+  title: 'category1',
+  imageUrl: 'https://reactjs.org/logo-og.png',
+};
+export const dummyCategory2 = {
+  title: 'category2',
+  imageUrl: 'https://reactjs.org/logo-og.png',
+};
+export const dummyCategory3 = {
+  title: 'category3',
+  imageUrl: 'https://reactjs.org/logo-og.png',
+};
+export const dummyCategory4 = {
+  title: 'category4',
+  imageUrl: 'https://reactjs.org/logo-og.png',
+};
+export const dummyCategory5 = {
+  title: 'category5',
+  imageUrl: 'https://reactjs.org/logo-og.png',
+};
+export const dummyCategory6 = {
+  title: 'category6',
+  imageUrl: 'https://reactjs.org/logo-og.png',
 };
 
-export const dummyProductWithDiscount = {
-    title : "Product Name",
-    imageUrl: 'https://reactjs.org/logo-og.png',
-    price : 100,
-    discount: 0.3,
-    desciption: "Very Nice Product From ABC etc.... "
-};
+export const dummyCategories = [
+  dummyCategory1,
+  dummyCategory2,
+  dummyCategory3,
+  dummyCategory4,
+  dummyCategory5,
+  dummyCategory6,
+];
 
-export const dummyProduct = {
-    title : "Product",
-    imageUrl: 'https://reactjs.org/logo-og.png',
-    price : 100,
+export const dummyProduct1 = {
+  title: 'Product1 Name',
+  imageUrl: 'https://reactjs.org/logo-og.png',
+  price: 100,
+  discount: 0.3,
+  desciption: 'Very Nice Product From ABC etc.... ',
 };
+export const dummyProduct2 = {
+  title: 'Product2 Name',
+  imageUrl: 'https://reactjs.org/logo-og.png',
+  price: 100,
+  discount: 0.3,
+  desciption: 'Very Nice Product From ABC etc.... ',
+};
+export const dummyProduct3 = {
+  title: 'Product3 Name',
+  imageUrl: 'https://reactjs.org/logo-og.png',
+  price: 100,
+  discount: 0.3,
+  desciption: 'Very Nice Product From ABC etc.... ',
+};
+export const dummyProduct4 = {
+  title: 'Product4 Name',
+  imageUrl: 'https://reactjs.org/logo-og.png',
+  price: 100,
+  discount: 0.3,
+  desciption: 'Very Nice Product From ABC etc.... ',
+};
+export const dummyProduct5 = {
+  title: 'Product5 Name',
+  imageUrl: 'https://reactjs.org/logo-og.png',
+  price: 100,
+  discount: 0.3,
+  desciption: 'Very Nice Product From ABC etc.... ',
+};
+export const dummyProduct6 = {
+  title: 'Product6 Name',
+  imageUrl: 'https://reactjs.org/logo-og.png',
+  price: 100,
+  discount: 0.3,
+  desciption: 'Very Nice Product From ABC etc.... ',
+};
+export const dummyProduct7 = {
+  title: 'Product7 Name',
+  imageUrl: 'https://reactjs.org/logo-og.png',
+  price: 100,
+  discount: 0.3,
+  desciption: 'Very Nice Product From ABC etc.... ',
+};
+export const dummyProduct8 = {
+  title: 'Product8 Name',
+  imageUrl: 'https://reactjs.org/logo-og.png',
+  price: 100,
+  discount: 0.3,
+  desciption: 'Very Nice Product From ABC etc.... ',
+};
+export const dummyProducts = [
+  dummyProduct1,
+  dummyProduct2,
+  dummyProduct3,
+  dummyProduct4,
+  dummyProduct5,
+  dummyProduct6,
+  dummyProduct7,
+  dummyProduct8,
+];
+
 export const dummycartItem = {
-    product: dummyProductWithDiscount
+  product: dummyProduct1,
 };
 export const dummyOrder = {
-    cartItems: [dummycartItem],
-    cost: 150,
-    status: 'Placed '
+  cartItems: [dummycartItem],
+  cost: 150,
+  status: 'Placed ',
 };

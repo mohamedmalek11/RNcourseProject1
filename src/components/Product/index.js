ab7ea53ba94d24e22e7f4c103111a772cd2b10ac
Mohamed Malek <mohamed.khatwa.zaid@gmail.com>
2020-09-09 08:05:55 +0200

    Added useNavigation

--- src/components/Product/index.js
+++ src/components/Product/index.js
@@ -1,24 +1,28 @@
 import React from 'react';
 import {Text, View, Image} from 'react-native';
+import {useNavigation} from '@react-navigation/native';
+import {PlatformTouchable} from '../PlatformTouchable';
 import {Card} from '../Card';
 import {Price} from '../Price';
 import styles from './style';
 
 export function Product(props) {
   const {product} = props;
-  return (
-    <View style={styles.container}>
+  const navigation = useNavigation();
 
+  return (
+    <PlatformTouchable
+      style={styles.container}
+      onPress={() => navigation.navigate('ProductScreen')}>
       <View style={styles.imageWrapper}>
         <Card>
           <Image style={styles.imageStyle} source={{uri: product.imageUrl}} />
         </Card>
       </View>
       <View style={styles.titletWrapper}>
-        <Text style = {styles.ProductTitle}>{product.title}</Text>
+        <Text style={styles.ProductTitle}>{product.title}</Text>
         <Price price={product.price} discount={product.discount} />
       </View>
-
-    </View>
+    </PlatformTouchable>
   );
 }

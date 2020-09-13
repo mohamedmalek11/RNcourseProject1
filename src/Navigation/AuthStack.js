b63910cdf69e2dda423dcc994bb43a7d935045ab
Mohamed Malek <mohamed.khatwa.zaid@gmail.com>
2020-09-13 08:09:03 +0200

    Added Options

--- src/Navigation/AuthStack.js
+++ src/Navigation/AuthStack.js
@@ -16,6 +16,7 @@ export function AuthStack(props) {
       <Stack.Screen
         name="ConfirmationCodeScreen"
         component={ConfirmationCodeScreen}
+        options = {{headerShown: false}}
       />
     </Stack.Navigator>
   );

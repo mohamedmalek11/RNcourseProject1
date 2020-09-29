e0c4fa4cd5319f7981a503a31f30f8025a1271c0
Mohamed Malek <mohamed.khatwa.zaid@gmail.com>
2020-09-29 17:23:32 +0200

    setup BaseUrl

--- index.js
+++ index.js
@@ -2,5 +2,7 @@ import 'react-native-gesture-handler';
 import {AppRegistry} from 'react-native';
 import App from './App';
 import {name as appName} from './app.json';
+import {configurAxios} from './src/utils/helperFunctions'
 
+configurAxios();
 AppRegistry.registerComponent(appName, () => App);
